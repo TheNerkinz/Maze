8ee8263b47a91513ed819a878e8a1daa17d21cee
TheNerkinz <igorekaterinamushta@gmail.com>
2021-11-28 20:05:33 +0300

    realese commit 1.0

---
+++ Assets/material/ColliderSceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ColliderSceneLoader : MonoBehaviour {
+
+    public string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
